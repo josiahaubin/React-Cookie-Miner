1147560f247a1f1818fdd33d204360b1695aa95d
josiahaubin <josiahaubin@gmail.com>
2021-07-08 16:49:03 -0600

    Added some multiplier names and speeds titles

--- src/Components/CookieSheet.js
+++ src/Components/CookieSheet.js
@@ -4,14 +4,24 @@ import { useState } from "react";
 import { Row } from "react-bootstrap";
 import Col from 'react-bootstrap/Col'
 
-function CookiePan() {
+function CookieSheet() {
     const [numberOfCookies, setNumberOfCookies] = useState(0);
     const [multiplier, setMultiplier] = useState(1);
 
     return (
         <div>
             <Row>
-                <Col xs={10}>
+                <Col xs={2}>
+                    {/* Sidebar */}
+                    <div>
+                        <h5>Sibling</h5>
+                        <h5>Friend</h5>
+                        <h5>Dad</h5>
+                        <h5>Mom</h5>
+                        <h5>Grandma</h5>
+                    </div>
+                </Col>
+                <Col xs={8}>
                     {/* Main */}
                     <div className="cookieArea">
                         <h1>Number of Cookies: {numberOfCookies}</h1>
@@ -21,7 +31,11 @@ function CookiePan() {
                 <Col xs={2}>
                     {/* Sidebar */}
                     <div>
-
+                        <h5>Rolling Pin</h5>
+                        <h5>Picaxe</h5>
+                        <h5>Drill</h5>
+                        <h5>Plasma Cutter</h5>
+                        <h5>Lightsaber</h5>
                     </div>
                 </Col>
             </Row>
@@ -30,4 +44,4 @@ function CookiePan() {
     )
 }
 
-export default CookiePan;
+export default CookieSheet;
