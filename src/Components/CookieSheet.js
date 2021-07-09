d62a9e4f12f6bd3f697cc326d7f9c2df4721b6f3
josiahaubin <josiahaubin@gmail.com>
2021-07-09 15:45:05 -0600

    Added bootstrap cards for multipliers

--- src/Components/CookieSheet.js
+++ src/Components/CookieSheet.js
@@ -1,8 +1,7 @@
 import cookiePic from "../Media/cookie.png"
 import "../CSS/CookieSheet.css"
 import { useState } from "react";
-import { Row } from "react-bootstrap";
-import Col from 'react-bootstrap/Col'
+import { Row, Col, Card, Button } from "react-bootstrap";
 
 function CookieSheet() {
     const [numberOfCookies, setNumberOfCookies] = useState(0);
@@ -31,16 +30,65 @@ function CookieSheet() {
                 <Col xs={2}>
                     {/* Sidebar */}
                     <div>
-                        <h5>Rolling Pin</h5>
-                        <h5>Picaxe</h5>
-                        <h5>Drill</h5>
-                        <h5>Plasma Cutter</h5>
-                        <h5>Lightsaber</h5>
+                        {/* Rolling Pin */}
+                    <Card className="cardColor" border="light">
+                        <Card.Img variant="top" src="" />
+                        <Card.Body>
+                            <Card.Title>Rolling Pin</Card.Title>
+                            <Card.Text>
+                                Helps you chip away at the cookie. +1 per click. <br/> <b>Cost: 20 Cookies</b>
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                        {/* Pickaxe */}
+                    <Card className="cardColor" border="light">
+                        <Card.Img variant="top" src="" />
+                        <Card.Body>
+                            <Card.Title>Pickaxe</Card.Title>
+                            <Card.Text>
+                                Helps you chip away at the cookie. +1 per click. <br/> <b>Cost: 20 Cookies</b>
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                        {/* Drill */}
+                    <Card className="cardColor" border="light">
+                        <Card.Img variant="top" src="" />
+                        <Card.Body>
+                            <Card.Title>Drill</Card.Title>
+                            <Card.Text>
+                                Helps you chip away at the cookie. +1 per click. <br/> <b>Cost: 20 Cookies</b>
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                        {/* Plasma Cutter */}
+                    <Card className="cardColor" border="light">
+                        <Card.Img variant="top" src="" />
+                        <Card.Body>
+                            <Card.Title>Plasma Cutter</Card.Title>
+                            <Card.Text>
+                                Helps you chip away at the cookie. +1 per click. <br/> <b>Cost: 20 Cookies</b>
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                        {/* Lightsaber */}
+                    <Card className="cardColor" border="light">
+                        <Card.Img variant="top" src="" />
+                        <Card.Body>
+                            <Card.Title>Lightsaber</Card.Title>
+                            <Card.Text>
+                                Helps you chip away at the cookie. +1 per click. <br/> <b>Cost: 20 Cookies</b>
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
                     </div>
                 </Col>
             </Row>
         </div>
-        
     )
 }
 
